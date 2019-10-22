a2dd449bb51ac20f5a4b0d3243608a830caead1d
hchalla2021 <53994609+hchalla2021@users.noreply.github.com>
2019-10-22 21:34:53 +0530

    This is first 2nd commit from github

--- BankingApp/src/main/java/GTPLB/BankingApp/Github.java
+++ BankingApp/src/main/java/GTPLB/BankingApp/Github.java
@@ -5,7 +5,7 @@ public class Github {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-     System.out.println("Hello sravanthi...");
+         System.out.println("Hello sravanthi this is first github Project");
      
 	}
 
