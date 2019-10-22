c4c2fcc7443251ae2b4d5ca510fd63b01d78f016
harikrishna <hchalla2020@gmail.com>
2019-10-22 21:32:55 +0530

    This is xyz bank first commit

---
+++ BankingApp/src/main/java/GTPLB/BankingApp/Github.java
@@ -0,0 +1,12 @@
+package GTPLB.BankingApp;
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     System.out.println("Hello sravanthi...");
+     
+	}
+
+}
